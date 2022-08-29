2892912ef86746214c3ce2bbbc820fe4eb791cc3
RuiZhang <zhangrui_19@163.com>
2022-08-29 10:00:49 +0800

    基础模块命名

---
+++ db/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2022/8/29 9:59
+# @Author   : dapwn
+# @File     : __init__.py.py
+# @Software : PyCharm
+# @Function :
---
+++ spider/yfb.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2022/8/29 9:59
+# @Author   : dapwn
+# @File     : yfb.py
+# @Software : PyCharm
+# @Function : 乙方宝爬虫程序
